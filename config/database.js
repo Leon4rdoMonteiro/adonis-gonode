4861f60573cccfa89d94dd3da26c9725247400cb
Leon4rdoMonteiro <leonardomonteiro.sh@gmail.com>
2020-09-16 21:40:59 -0300

    database-config-updated

--- config/database.js
+++ config/database.js
@@ -76,8 +76,8 @@ module.exports = {
       host: Env.get('DB_HOST', 'localhost'),
       port: Env.get('DB_PORT', ''),
       user: Env.get('DB_USER', 'root'),
-      password: Env.get('DB_PASSWORD', ''),
-      database: Env.get('DB_DATABASE', 'adonis')
+      password: Env.get('DB_PASS', ''),
+      database: Env.get('DB_NAME', 'adonis')
     },
     debug: Env.get('DB_DEBUG', false)
   }
