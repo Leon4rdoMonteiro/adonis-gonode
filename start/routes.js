55d6fc46a14a1008f5d7843662f51cf999b31b3a
Leon4rdoMonteiro <leonardomonteiro.sh@gmail.com>
2020-09-16 21:48:58 -0300

    social-authentication-init

--- start/routes.js
+++ start/routes.js
@@ -2,12 +2,32 @@
 
 const Route = use('Route')
 
+// Social Authentication
+
+Route.get('facebook', async ({ ally }) => {
+  await ally.driver('facebook').redirect()
+})
+
+Route.get('facebook/authenticated', async ({ ally }) => {
+  const user = await ally.driver('facebook').getUser()
+
+  return user
+})
+
+// Default Sessions
+
 Route.post('sessions', 'SessionController.store')
+
+// Password recovery
 
 Route.post('passwords', 'ForgotPasswordController.store')
 Route.put('passwords', 'ForgotPasswordController.update')
 
+// Users
+
 Route.post('users', 'UserController.store')
+
+// Files
 
 Route.post('files', 'FileController.store')
 Route.get('files/:id', 'FileController.show')
